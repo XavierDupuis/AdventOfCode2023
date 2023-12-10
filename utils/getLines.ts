5d9a361468b5121e471d0120205f9b469f2e6d3f
XavierDupuis <xavier0978@hotmail.fr>
2023-12-10 15:01:12 -0500

    Update getLines.ts

--- utils/getLines.ts
+++ utils/getLines.ts
@@ -1,8 +1,15 @@
 import * as fs from "fs";
 import { Day } from "./days";
+import { exit } from "process";
 
 export function get(day: Day) {
-    return fs.readFileSync(`./days/${day}/input.txt`, "utf8");
+    try {
+        return fs.readFileSync(`./days/${day}/input.txt`, "utf8");
+    } catch (error) {
+        console.error(`> Error reading input file for day ${day}`);
+        console.error(`> Verify that the file '/days/${day}/input.txt' exists.`);
+        exit(1);
+    }
 }
 
 export function getLines(day: Day) {
