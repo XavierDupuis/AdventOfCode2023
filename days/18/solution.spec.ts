fa995c0579a2ac8d58d6f1bbcd39b41b7663cdf5
XavierDupuis <xavier0978@hotmail.fr>
2023-12-20 15:35:30 -0500

    day 18 part 1 (shoelace + pick)

---
+++ days/18/solution.spec.ts
@@ -0,0 +1,29 @@
+import { part1, part2 } from './solution';
+
+describe('Day 18', () => {
+    const digPlan = [
+        'R 6 (#70c710)',
+        'D 5 (#0dc571)',
+        'L 2 (#5713f0)',
+        'D 2 (#d2c081)',
+        'R 2 (#59c680)',
+        'D 2 (#411b91)',
+        'L 5 (#8ceee2)',
+        'U 2 (#caa173)',
+        'L 1 (#1b58a2)',
+        'U 2 (#caa171)',
+        'R 2 (#7807d2)',
+        'U 3 (#a77fa3)',
+        'L 2 (#015232)',
+        'U 2 (#7a21e3)'
+    ]
+
+    describe('Part 1', () => {
+        it('should return 62 the number of cubic meters of lava', () => {
+            expect(part1(digPlan)).toBe(62);
+        });
+    });
+
+    describe('Part 2', () => {
+    });
+});
---
+++ days/18/solution.ts
@@ -0,0 +1,104 @@
+import { solutionner } from "../../utils/solutionner";
+import { Day } from "../../utils/days";
+
+enum Direction {
+    Up = 'U',
+    Down = 'D',
+    Left = 'L',
+    Right = 'R'
+}
+
+interface Coordinates {
+    i: number;
+    j: number;
+}
+
+const NextCoordinates: { [key in Direction]: Coordinates } = {
+    [Direction.Up]: { i: -1, j: 0 },
+    [Direction.Down]: { i: 1, j: 0 },
+    [Direction.Left]: { i: 0, j: -1 } ,
+    [Direction.Right]: { i: 0, j: 1 }
+}
+
+interface DigInstruction {
+    direction: Direction;
+    distance: number;
+    color: string;
+}
+
+type DigPlan = DigInstruction[];
+
+function parseDigPlan(lines: string[]): DigPlan {
+    return lines.map((line) => line.split(' ')).map(([direction, distance, color]) => 
+        ({ direction: direction as Direction, distance: parseInt(distance), color })
+    );
+}
+
+function getNextCoordinates({i: previousI, j: previousJ }: Coordinates, direction: Direction, distance: number): Coordinates {
+    const { i: deltaI, j: deltaJ } = NextCoordinates[direction];
+    const i = previousI + deltaI * distance;
+    const j = previousJ + deltaJ * distance;
+    return { i, j };
+}
+
+function getPolygonPoints(digPlan: DigPlan): Coordinates[] {
+    const points: Coordinates[] = [];
+    let current: Coordinates = { i: 0, j: 0 };
+    digPlan.forEach(({ direction, distance }) => {
+        points.push(current);
+        current = getNextCoordinates(current, direction, distance);
+    });
+    return points;
+}
+
+// https://en.wikipedia.org/wiki/Shoelace_formula
+function getPolygonArea(points: Coordinates[]): number {
+    let area = 0;
+    for (let k = 0; k < points.length; k++) {
+        const k1 = (k) % points.length;
+        const k2 = (k + 1) % points.length;
+        const { i: i1, j: j1 } = points[k1];
+        const { i: i2, j: j2 } = points[k2];
+        area += (i1 * j2) - (i2 * j1);
+    }
+    area = Math.abs(area) / 2;
+    return area;
+}
+
+// https://en.wikipedia.org/wiki/Pick%27s_theorem
+function getInternalPointCount(area: number, boundaryPointsCount: number): number {
+    // A = i + b/2 - 1 
+    // i = A - b/2 + 1
+    return area - boundaryPointsCount / 2 + 1;
+}
+
+function getDistance(p1: Coordinates, p2: Coordinates): number {
+    return Math.sqrt(Math.pow(p2.i - p1.i, 2) + Math.pow(p2.j - p1.j, 2));
+}
+
+function getPolygonPerimeter(points: Coordinates[]): number {
+    let perimeter = 0;
+    for (let k = 0; k < points.length; k++) {
+        const k1 = (k) % points.length;
+        const k2 = (k + 1) % points.length;
+        perimeter += getDistance(points[k1], points[k2]);
+    }
+    return perimeter;
+}
+
+function part1(lines: string[]): number {
+    const digPlan = parseDigPlan(lines);
+    const points = getPolygonPoints(digPlan);
+    const area = getPolygonArea(points);
+    const boundaryPointsCount = getPolygonPerimeter(points);
+    const internalPointsCount = getInternalPointCount(area, boundaryPointsCount);
+    const totalArea = internalPointsCount + boundaryPointsCount;
+    return totalArea;
+}
+
+function part2(lines: string[]): number {
+    return 0;
+}
+
+solutionner(Day.D18, part1, part2);
+export { part1, part2 };
